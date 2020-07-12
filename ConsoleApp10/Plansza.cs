dc9451e3dddc4998f742754c2b6a0eb0c962cfa8
ewa-marchewa <61653171+ewa-marchewa@users.noreply.github.com>
2020-07-12 20:37:09 +0200

    dodano klasy

---
+++ ConsoleApp10/Plansza.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace ConsoleApp10
+{
+    public class Plansza
+    {
+        public int[,] Gracz  {get; set;}
+
+        public Plansza(int[,] gracz)
+        {
+            Gracz = gracz  ;
+        }
+    }
+}
---
+++ ConsoleApp10/Statek.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace ConsoleApp10
+{
+    public class Statek
+    {
+        public int Kolumna_startowa { get; set; }
+        public int Wiersz_startowy { get; set; }
+
+        
+
+    // public int Dlugosc_statku
+    //  {
+    //     get => Dlugosc_statku; set { if (Dlugosc_statku > 2 && Dlugosc_statku < 0) { throw new ArgumentException("Zły wymiar statku"); } }
+    // }
+
+    public Statek(int kolumna_startowa, int wiersz_startowy)//, int dlugosc_statku)
+        {
+            Kolumna_startowa = kolumna_startowa;
+            Wiersz_startowy = wiersz_startowy;
+            //Dlugosc_statku = dlugosc_statku;
+        }
+        public override string ToString()
+        {
+            return "kolumna " + Kolumna_startowa + " wierszu: " + Wiersz_startowy + " punktów życia";
+        }
+
+    }
+
+}
